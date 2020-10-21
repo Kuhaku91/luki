106370955573004c0c23cc242bf579688bc90c7f
wildanfuady <wildanfuady@gmail.com>
2020-10-21 11:07:51 +0700

    add jam

--- app/src/main/java/com/wildanfuady/bookingservice/FinishBookingActivity.kt
+++ app/src/main/java/com/wildanfuady/bookingservice/FinishBookingActivity.kt
@@ -25,7 +25,21 @@ class FinishBookingActivity : AppCompatActivity() {
         }
         var rupiah = Converter.formatRupiah(harga.toInt())
         textHarga.setText(rupiah)
-        textNoAntrian.setText("Nomor Antrian: " + antrian)
+        var jam = ""
+        if(antrian == "1"){
+            jam = "08.00 - 09.00"
+        } else if(antrian == "2"){
+            jam = "09.00 - 10.00"
+        } else if(antrian == "3"){
+            jam = "10.00 - 11.00"
+        } else if(antrian == "4"){
+            jam = "11.00 - 12.00"
+        } else if(antrian == "5"){
+            jam = "13.00 - 14.00"
+        } else {
+            jam = ""
+        }
+        textNoAntrianAndJam.setText("No. Antrian: " + antrian + " Jam " + jam)
 
         btnBackToHome.setOnClickListener {
             backToHome()
